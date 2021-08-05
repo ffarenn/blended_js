9eef5a26f0a271e0a643ba079ab136157aafd6ce
Andrejs Klimza <andrejsklimza@gmail.com>
2021-08-05 21:27:03 +0100

    adds tasks and js files

---
+++ js/task2.js
@@ -0,0 +1,26 @@
+
+// const word = prompt('Please enter a word');
+// let result
+// console.log(word);
+
+// if (word === '') {
+//     result = 'Invalid value';
+// } else if (word === null) {
+//     result = 'Good luck';
+// } else if (word.trim() === '') {
+//     result = 'Invalid value'
+// }
+
+// alert(result);
+
+// if (word.length % 2 === 0) {
+//     result = Math.ceil(word.length / 2) + 
+// }
+
+function multiply(...args) {
+    console.log(args); // массив всех аргументов
+}
+
+multiply(1, 2);
+multiply(1, 2, 3);
+multiply(1, 2, 3, 4);
